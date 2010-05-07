8f1dec4bf7d6fd44b4996ac881c53a6c98008671
Caleb Brown <kayluhb@hash.(none)>
2010-05-07 09:28:38 -0400

    working on the models

---
+++ src/apps/abstract/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class CommonModel(models.Model):
+    """
+    This is an abstract model which will be inherited by nearly all models. When the object is created
+    it will get a date_created timestamp and each time it is modified it will recieve a date_modified
+    time stamp as well.
+    """
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
---
+++ src/local_settings_example.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/kayluhb/Projects/bagel/db/bagel.sqlite',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+    }
+}
+
